cfe42fd8ab33e6cfc46cb26ed4d4cfe57a9b8761
Pharap <pharap@hotmail.com>
2016-06-27 23:37:36 +0100

    Made `rotateLeft` and `rotateRight` wrap around

--- loot/direction.h
+++ loot/direction.h
@@ -11,10 +11,10 @@ enum class Direction : uint8_t
 
 Direction rotateRight(Direction dir)
 {
-  return static_cast<Direction>(static_cast<uint8_t>(dir) + 1);
+  return static_cast<Direction>((static_cast<uint8_t>(dir) + 1) % 4);
 }
 
 Direction rotateLeft(Direction dir)
 {
-  return static_cast<Direction>(static_cast<uint8_t>(dir) - 1);
+  return static_cast<Direction>((static_cast<uint8_t>(dir) - 1) % 4);
 }
