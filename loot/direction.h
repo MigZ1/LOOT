e09691289466bc39ce5815770bbeccc7d8ee11ce
Pharap <pharap@hotmail.com>
2016-06-27 23:32:03 +0100

    Added `rotateLeft` and `rotateRight` functions

--- loot/direction.h
+++ loot/direction.h
@@ -8,3 +8,13 @@ enum class Direction : uint8_t
   West = 2,
   North = 3
 }
+
+Direction rotateRight(Direction dir)
+{
+  return static_cast<Direction>(static_cast<uint8_t>(dir) + 1);
+}
+
+Direction rotateLeft(Direction dir)
+{
+  return static_cast<Direction>(static_cast<uint8_t>(dir) - 1);
+}
