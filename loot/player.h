59d058e3daefcd594c18a98382f8b6c11c6bd007
Pharap <pharap@hotmail.com>
2016-06-30 20:24:35 +0100

    Added `changeDirection`

--- loot/player.h
+++ loot/player.h
@@ -21,6 +21,7 @@ class Player
     char name[8];
 
     void init();
+    void changeDirection(const Direction direction);
     void move(const int8_t distance);
     void jump(const uint8_t x, const uint8_t y);
     void step();
